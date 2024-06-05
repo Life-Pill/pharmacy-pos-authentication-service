3fc360684a957a0de850d4a53358b45a27d64194
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-06-06 02:27:34 +0530

    authentication 2.7 version

--- src/main/java/com/pmj/authentication_service/service/EmployerService.java
+++
@@ -1,7 +0,0 @@
-package com.pmj.authentication_service.service;
-
-import com.pmj.authentication_service.dto.EmployerDTO;
-
-public interface EmployerService {
-    EmployerDTO getEmployerByUsername(String username);
-}
